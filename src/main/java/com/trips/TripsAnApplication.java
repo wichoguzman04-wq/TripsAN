8f365dbd059005e93542d17be5272ad39e145d18
Mauricio <wichoguzman04@gmail.com>
2026-03-28 11:10:59 -0600

    estructura del proyceto trip

---
+++ src/main/java/com/trips/TripsAnApplication.java
@@ -0,0 +1,13 @@
+package com.trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsAnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trips/TripsAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
