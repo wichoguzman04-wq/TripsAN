8b491ac2068555049863ec0c4cee28dd6615f297
Mauricio <wichoguzman04@gmail.com>
2026-05-01 18:45:18 -0600

    complemento del laboratorio 3

--- src/main/java/com/trips/controllers/HomeController.java
+++ src/main/java/com/trips/controllers/HomeController.java
@@ -10,110 +10,121 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.trips.models.Trip;
 import com.trips.services.ITripServices;
 
-
 @Controller
-
 public class HomeController {
-	
-	@Autowired
-	private ITripServices tripService;
-	 
-	@GetMapping("/")
-	public String mostrarHome (Model model) {
-		
-		String trip = "Rapel en el Volcan";
-		Date fechaPublicacion = new Date(); 
-		double costo = 5.0;
-		boolean vigente= true;
 
-		model.addAttribute ("trip", trip); 
-		model.addAttribute("fechaPublicacion", fechaPublicacion); 
-		model.addAttribute ("costo", costo); 
-		model.addAttribute ("vigente", vigente);
-		
-		return "home";
-	}
-	
-	@GetMapping("/tabla")
-	public String mostrarTabla(Model model) {
-	    List<Trip> lista = tripService.buscarTodo();
-	    model.addAttribute("trips", lista);
-	    return "tabla";
-	}
-	 
-	@GetMapping("/listado")
+    @Autowired
+    private ITripServices tripService;
+
+    @GetMapping("/")
+    public String mostrarHome(Model model) {
+        List<Trip> lista = tripService.buscarTodo();
+        model.addAttribute("trips", lista);
+        return "home";
+    }
+
+    @GetMapping("/tabla")
+    public String mostrarTabla(Model model) {
+        List<Trip> lista = tripService.buscarTodo();
+        model.addAttribute("trips", lista);
+        return "tabla";
+    }
+
+    @GetMapping("/listado")
     public String mostrarListado(Model model) {
         model.addAttribute("listadoTrips", getTrips());
         return "listado";
     }
 
-	private List<Trip> getTrips() {
-	    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-	    List<Trip> lista = new LinkedList<Trip>();
-	    try {
-	        Trip trip1 = new Trip();
-	        trip1.setId(1);
-	        trip1.setNomTrip("Rapel en Volcatenango");
-	        trip1.setDescripcion("Hacer rapel en los circuitos de Volcatenango");
-	        trip1.setFecha(sdf.parse("10-05-2022"));
-	        trip1.setCosto(5.0);
-	        trip1.setDestacado(1);
-	        trip1.setImagen("trip01.png");
-	      
+    @GetMapping("/detalle/{id}")
+    public String mostrarDetalle(@PathVariable("id") int id, Model model) {
+        Trip trip = new Trip();
+        trip.setId(id);
+        trip.setNomTrip("Rapel en Volcatenango");
+        trip.setDescripcion("Aventa rapel en un circuito conectado en las...");
+        trip.setFecha(new Date());
+        trip.setCosto(10.0);
 
-	        Trip trip2 = new Trip();
-	        trip2.setId(2);
-	        trip2.setNomTrip("Deslizadero en El picnic");
-	        trip2.setDescripcion("Deslizarte en un divertido tobogan sobre la colina");
-	        trip2.setFecha(sdf.parse("10-05-2022"));
-	        trip2.setCosto(5.0);
-	        trip2.setDestacado(1);
-	        trip2.setImagen("trip02.png");
+        model.addAttribute("trip", trip);
 
-	        Trip trip3 = new Trip();
-	        trip3.setId(3);
-	        trip3.setNomTrip("Comida y Flores");
-	        trip3.setDescripcion("Disfrutar de un amplio jardin");
-	        trip3.setFecha(sdf.parse("10-05-2022"));
-	        trip3.setCosto(1.0);
-	        trip3.setDestacado(0); 
-	        trip3.setImagen("tirp03.png");
+        return "trips/detalle";
+    }
 
-	        Trip trip4 = new Trip();
-	        trip4.setId(4);
-	        trip4.setNomTrip("Caminatas");
-	        trip4.setDescripcion("Disfruta hacer senderismo por las montañas chalatecas");
-	        trip4.setFecha(sdf.parse("01-02-2022"));
-	        trip4.setCosto(1.0);
-	        trip4.setDestacado(0);
-	        trip4.setImagen("no-image.png");
+    @GetMapping("/eliminar/{id}")
+    public String eliminarTrip(@PathVariable("id") int id, Model model) {
+        model.addAttribute("mensajeEliminado", "El Id trip " + id + " ha sido eliminado");
+        return "mensaje";
+    }
 
-	        lista.add(trip1);
-	        lista.add(trip2);
-	        lista.add(trip3);
-	        lista.add(trip4);
+    @GetMapping("/mensaje")
+    public String mostrarMensaje() {
+        return "mensaje";
+    }
 
-	    } catch (ParseException e) {
-	        e.printStackTrace();
-	    }
-	    return lista;
-	}
-	
-	@GetMapping("/detalle")
-	public String mostrarDetalle(Model model) {
-		Trip trip = new Trip();
-		trip.setNomTrip("Rapel en Volcatenango");
-		trip.setDescripcion("Aventa rapel en un circuito conectado en las...");
-		trip.setFecha(new Date());
-		trip.setCosto(10.0);
-		model.addAttribute("trip" , trip);
-		
-		return "detalle";
-	}
-	
-	
+    @GetMapping("/formCategoria")
+    public String mostrarFormularioCategoria() {
+        return "categoriasTrip/formCategoria";
+    }
+
+    @GetMapping("/listCategoria")
+    public String mostrarListadoCategorias() {
+        return "categoriasTrip/listCategoria";
+    }
+
+    private List<Trip> getTrips() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        List<Trip> lista = new LinkedList<Trip>();
+        try {
+            Trip trip1 = new Trip();
+            trip1.setId(1);
+            trip1.setNomTrip("Rapel en Volcatenango");
+            trip1.setDescripcion("Hacer rapel en los circuitos de Volcatenango");
+            trip1.setFecha(sdf.parse("10-05-2022"));
+            trip1.setCosto(5.0);
+            trip1.setDestacado(1);
+            trip1.setImagen("trip01.png");
+
+            Trip trip2 = new Trip();
+            trip2.setId(2);
+            trip2.setNomTrip("Deslizadero en El picnic");
+            trip2.setDescripcion("Deslizarte en un divertido tobogan sobre la colina");
+            trip2.setFecha(sdf.parse("10-05-2022"));
+            trip2.setCosto(5.0);
+            trip2.setDestacado(1);
+            trip2.setImagen("trip02.png");
+
+            Trip trip3 = new Trip();
+            trip3.setId(3);
+            trip3.setNomTrip("Comida y Flores");
+            trip3.setDescripcion("Disfrutar de un amplio jardin");
+            trip3.setFecha(sdf.parse("10-05-2022"));
+            trip3.setCosto(1.0);
+            trip3.setDestacado(0);
+            trip3.setImagen("trip03.png");
+
+            Trip trip4 = new Trip();
+            trip4.setId(4);
+            trip4.setNomTrip("Caminatas");
+            trip4.setDescripcion("Disfruta hacer senderismo por las montañas chalatecas");
+            trip4.setFecha(sdf.parse("01-02-2022"));
+            trip4.setCosto(1.0);
+            trip4.setDestacado(0);
+            trip4.setImagen("no-image.png");
+
+            lista.add(trip1);
+            lista.add(trip2);
+            lista.add(trip3);
+            lista.add(trip4);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
 }
